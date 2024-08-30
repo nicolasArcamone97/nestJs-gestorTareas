de9224bca1c99cef5bbe7846bc9bf321a10605f4
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-30 00:32:01 -0300

    configuracion de cors para front

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,13 @@ async function bootstrap() {
     transform:true
   }))
 
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4200', // Reemplaza con la URL de tu frontend
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions)
 
   await app.listen(3000);
 }
