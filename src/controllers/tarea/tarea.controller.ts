652034933ccd8645e9a8ce817947f5919c809fb8
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-29 23:46:44 -0300

    creado de service y controller

--- src/controllers/tarea/tarea.controller.ts
+++ src/controllers/tarea/tarea.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TareaDTO } from 'src/dtos/tarea.dto';
+import { Tarea } from 'src/entities/tarea.entity';
+import { TareaService } from 'src/services/tarea/tarea.service';
 
 @Controller('tarea')
-export class TareaController {}
+export class TareaController {
+
+    constructor(private readonly tareaService:TareaService){}
+
+    @Get()
+    getTareas():Promise<Tarea[]>{
+        return this.tareaService.getTareas()
+    }
+
+
+    @Get(':id')
+    getTarea(@Param('id') id:number):Promise<TareaDTO>{
+        return this.tareaService.getTarea(id)
+    }
+
+
+    @Post()
+    crearTarea(@Body() tarea:TareaDTO):Promise<TareaDTO>{
+        return this.tareaService.crearTarea(tarea)
+    }
+
+
+}
--- src/services/tarea/tarea.service.ts
+++ src/services/tarea/tarea.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TareaDTO } from 'src/dtos/tarea.dto';
+import { Tarea } from 'src/entities/tarea.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TareaService {}
+export class TareaService {
+
+    constructor(@InjectRepository(Tarea) private readonly  tareaRepository:Repository<Tarea>){}
+
+
+
+    public async getTareas():Promise<Tarea[]>{
+        const tareas: Tarea[] = await this.tareaRepository.find()
+        return tareas
+    }
+
+    public async getTarea(idTarea:number):Promise<TareaDTO> {
+        const tarea: TareaDTO = await this.tareaRepository.findOne({where: {id:idTarea}}) 
+        
+        if(!tarea){
+            throw new NotFoundException("El id buscado no existe")
+        }
+
+        return tarea
+    }
+
+    public async crearTarea(tarea:TareaDTO):Promise<TareaDTO>{
+        const nuevaTarea:TareaDTO = await this.tareaRepository.save(tarea)
+
+        return nuevaTarea
+    }
+
+
+
+
+
+}
